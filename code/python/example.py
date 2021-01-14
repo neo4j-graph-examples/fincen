2242e7d7bd996bd31796d32c61b21e8956bdedbb
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2021-01-14 15:37:58 +0000

    Updating sandbox fincen

--- code/python/example.py
+++ code/python/example.py
@@ -4,7 +4,7 @@
 from neo4j import GraphDatabase, basic_auth
 
 driver = GraphDatabase.driver(
-  "bolt://<HOST>:<BOLTPORT>", 
+  "bolt://<HOST>:<BOLTPORT>",
   auth=basic_auth("<USERNAME>", "<PASSWORD>"))
 
 cypher_query = '''
@@ -17,8 +17,7 @@ RETURN from.name as originator
 with driver.session(database="neo4j") as session:
   results = session.read_transaction(
     lambda tx: tx.run(cypher_query,
-      country="Russia").data())
-
+                      country="Russia").data())
   for record in results:
     print(record['originator'])
 
